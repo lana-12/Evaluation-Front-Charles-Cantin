1b3e3247974a89b4946ba0159a9547f01eba4369
Virginie Giacometti <lana.12@wanadoo.fr>
2022-07-30 21:46:15 +0200

    add files .njk

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,10 @@
 
 module.exports = function(eleventyConfig){
     /* Tout ce qui est ds le src est cloné ds public */
-        eleventyConfig.addPassthroughCopy("./src/img")
-        eleventyConfig.addPassthroughCopy("./src/css")
-        eleventyConfig.addPassthroughCopy("./src/javascript")
-        eleventyConfig.addPassthroughCopy("./src/lien")
+        eleventyConfig.addPassthroughCopy("src/img")
+        eleventyConfig.addPassthroughCopy("src/css")
+        eleventyConfig.addPassthroughCopy("src/javascript")
+        eleventyConfig.addPassthroughCopy("src/lien")
     
         return{
             dir: {
